d0d43614299c9e31774ace5c15c73a08210cafa5
Flavio Ribeiro <flavio.ribeiro.3.42@gmail.com>
2017-03-21 03:11:50 -0300

    google maps apk

--- map_test/src/pages/home/home.ts
+++ map_test/src/pages/home/home.ts
@@ -1,15 +1,60 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { MenuController } from 'ionic-angular';
+
+import { Geolocation } from 'ionic-native';
+
+declare var google;
+
 @Component({
-  selector: 'page-home',
+  selector: 'home-page',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
+  constructor(public menuCtrl: MenuController, public navCtrl: NavController) {}
+
+  openMenu() {
+    this.menuCtrl.open();
   }
 
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+ 
+  loadMap(){
+ 
+    Geolocation.getCurrentPosition().then((position) => {
+ 
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+ 
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+ 
+    }, (err) => {
+      
+      console.log(err);
+
+      let latLng = new google.maps.LatLng(-22.9029383, -43.1766053);
+ 
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+ 
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    });
+
+  }
 }
